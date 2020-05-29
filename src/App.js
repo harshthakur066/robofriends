d0f8e304b737702e52a55fe84fa969a7ac4e23e4
harshthakur066 <harshthakur066@gmail.com>
2020-05-29 23:36:03 +0530

    formated the file structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
-import Header from "./Header";
+import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
+import Scroll from "./components/Scroll";
+import Header from "./components/Header";
 import "./App.css";
 
 const robots = [];
